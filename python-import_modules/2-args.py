89fa6b9434f2110e07f8d6d15f1650344da0766e
AymericLPR <mric31400@gmail.com>
2023-01-18 02:09:39 -0800

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     n_arg = len(sys.argv) - 1
     if n_arg == 1:
         print("1 arguments:")
-        print("1: {}".format(sys.argv[i]))
+        for i in range(1, n_arg + 1):
+            print("1: {}".format(sys.argv[i]))
     if n_arg > 1:
         print("{} arguments:".format((n_arg)))
         for i in range(1, n_arg + 1):
