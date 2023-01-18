2a0537692463dae6407ba2ff77dd3e83ef147ded
AymericLPR <mric31400@gmail.com>
2023-01-18 02:39:24 -0800

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    x = 0
     n_num = len(sys.argv) - 1
     if n_num == 1:
         for i in range(1, n_num + 1):
             print("{}".format(i))
     elif n_num > 1:
         for i in range(1, n_num + 1):
-            i = i + i
-            print("{}".format(i))
+            x = x + i
+        print("{}".format(x))
     else:
         print("0")
