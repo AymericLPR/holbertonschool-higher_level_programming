81001681abf9aac3818ec7077362d788e5996803
AymericLPR <mric31400@gmail.com>
2023-01-18 02:35:47 -0800

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if n_num == 1:
         for i in range(1, n_num + 1):
             print("{}".format(i))
-    elif n_arg > 1:
-        for i in range(1, n_arg + 1):
+    elif n_num > 1:
+        for i in range(1, n_num + 1):
             i = i + i
             print("{}".format(i))
     else:
