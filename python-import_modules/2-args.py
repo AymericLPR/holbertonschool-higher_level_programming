2afc3c841b75faba8f56c870b70e49fd785a2b30
AymericLPR <mric31400@gmail.com>
2023-01-18 02:00:45 -0800

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n_arg = len(sys.argv) - 1
+    if n_arg > 0:
+        print("{} arguments:".format((n_arg)))
+        for i in range(1, n_arg + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+    else:
+        print("0 arguments.")
