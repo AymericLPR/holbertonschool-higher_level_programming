437fa391bce8a3f55168e88740183bb21731d55a
AymericLPR <mric31400@gmail.com>
2023-01-19 01:25:44 -0800

    task 4

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    word = dir(hidden_4)
+    for i in range(word):
+        if word[0] == "_":
+            continue
+        print("{}".format(word))
