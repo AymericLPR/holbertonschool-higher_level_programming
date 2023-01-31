08d4a285bd8c796eaac82cc246d4770221a16fee
AymericLPR <mric31400@gmail.com>
2023-01-31 02:12:36 -0800

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This is a file of a empty class square """
+
+
+class Square:
+    """Empty class"""
+    def __init__(self, size=0):
+        """initialisation of values"""
+        self.__size = size
+
+        def square():
+            """empty instance"""
+            pass
