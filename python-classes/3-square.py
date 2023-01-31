776977ab7cd49197c4428a17b17b963ccc3251e4
AymericLPR <mric31400@gmail.com>
2023-01-31 02:24:07 -0800

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,10 +8,11 @@ class Square:
         """initialisation of values"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
-        def square(self):
+        def area(self):
             """ Return square area """
             return self.__size * self.__size
