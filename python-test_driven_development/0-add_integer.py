bf62c3a0905e7501af2b27817f000e2d82056066
AymericLPR <mric31400@gmail.com>
2023-02-03 00:57:13 -0800

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ Doc """
 
-
 def add_integer(a, b=98):
     """ Doc """
-    if (not isinstance(a, (float, int))):
+    if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
-    elif (not isinstance(b, (float, int))):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    else:
-        return int(a) + int(b)
-    result == a + b
+    result = a + b
     if result == float('inf') or result == -float('inf'):
         return 89
+    try:
+        return int(a) + int(b)
+    except ValueError:
+        return 0
