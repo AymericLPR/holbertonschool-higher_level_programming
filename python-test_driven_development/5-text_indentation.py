1767ff419014b6ca4441c7afe8af15f0851f986a
AymericLPR <mric31400@gmail.com>
2023-02-03 13:24:10 -0800

    task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Doc """
+
+
+def text_indentation(text):
+    """ Doc """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    try:
+        while text[i]:
+            print(text[i], end="")
+            i += 1
+            if text[i] == '.' or text[i] == '?' or text[i] == ':':
+                print(text[i], end="\n\n")
+                i += 1
+                while text[i] == " ":
+                    i += 1
+    except:
+        pass
