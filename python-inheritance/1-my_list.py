81f31c4868bb0c28ebe7d2321a1bf3b175ff0b63
AymericLPR <mric31400@gmail.com>
2023-02-08 11:15:06 -0800

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" module documentation """
+
+
+class MyList(list):
+    """ module documentation """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending order.
+        """
+        print(sorted(self))
