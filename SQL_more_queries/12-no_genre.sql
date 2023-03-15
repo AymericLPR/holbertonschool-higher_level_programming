f13d46386a68df2c29dcfb4bd81791881f48d6d0
AymericLPR <mric31400@gmail.com>
2023-03-15 13:44:45 -0700

    task 12

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Left join no match in right
+SELECT tv_shows.title, tv_show_genres.genre_id
+    FROM tv_shows
+    LEFT JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+        WHERE tv_show_genres.show_id IS NULL
+
+    ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
