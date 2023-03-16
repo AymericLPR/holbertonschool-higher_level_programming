2d5e76363c0e929a1598b6f385c033567ecbdd93
AymericLPR <mric31400@gmail.com>
2023-03-16 02:06:55 -0700

    task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- List all the cities in the database
 SELECT cities.id, cities.name, states.name
     FROM cities
-    INNER JOIN states ON state_id = cities.states.id
+    INNER JOIN states ON cities.state_id = states.id
