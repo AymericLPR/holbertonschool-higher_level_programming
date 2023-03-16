7464b13e317e08ba6c1789fe8f129fcb415b6ca8
AymericLPR <mric31400@gmail.com>
2023-03-16 02:03:45 -0700

    task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,4 @@
 USE hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name
     FROM cities
-    INNER JOIN states ON cities.state_id = states.id
+    INNER JOIN states ON state_id = cities.states.id
