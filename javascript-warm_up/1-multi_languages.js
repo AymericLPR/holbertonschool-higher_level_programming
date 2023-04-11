c035ef0e14f0f867a31111b088823838e9db5db6
AymericLPR <mric31400@gmail.com>
2023-04-11 01:47:21 -0700

    task 1

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const c = 'C is fun';
+const p = 'Python is cool';
+const j = 'JavaScript is amazing';
+
+const lines = [c, p, j];
+lines.forEach((line) => {
+  console.log(line);
+});
