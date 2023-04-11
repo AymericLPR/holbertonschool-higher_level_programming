788b929fb59929e140bc2c8505b7e7d3afe9177a
AymericLPR <mric31400@gmail.com>
2023-04-11 02:32:34 -0700

    add a file for the task 6 and first try

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const c = 'C is fun';
+const p = 'Python is cool';
+const j = 'JavaScript is amazing';
+
+const lines = [c, p, j];
+let i = 0;
+while (i < lines.length) {
+  console.log(lines[i]);
+  i++;
+}
