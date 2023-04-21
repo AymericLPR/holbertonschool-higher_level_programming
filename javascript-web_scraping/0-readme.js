b6e59d3da85a4d2520b33200d6f3cad3c72b9d5d
AymericLPR <mric31400@gmail.com>
2023-04-21 00:40:40 -0700

    update with spaces

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+
 const filename = process.argv[2];
+
 fs.readFile(filename, 'utf8', function (err, data) {
   if (err) {
     console.error(`Error reading file: ${err}`);
