5f26b08686314d2ca4fbd716739a8d04441d9a4f
AymericLPR <mric31400@gmail.com>
2023-04-21 02:31:10 -0700

    new file task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const process = require('process');
+
+const dict = {};
+request(process.argv[2], function (error, response, body) {
+  if (error) throw error;
+  JSON.parse(body).forEach(element => {
+    if (element.completed === true) {
+      if (dict[element.userId] === undefined) {
+        dict[element.userId] = 1;
+      } else { dict[element.userId] = dict[element.userId] += 1; }
+    }
+  });
+  console.log(dict);
+});
